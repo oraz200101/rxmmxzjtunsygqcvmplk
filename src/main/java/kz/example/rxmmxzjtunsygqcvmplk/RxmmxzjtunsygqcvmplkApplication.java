1e1ceb08bdb8ef69badfb183ebc690b775947f29
oraz200101 <oraz20010101@gmail.com>
2023-10-30 16:34:05 +0600

    first commit with dependencies

---
+++ src/main/java/kz/example/rxmmxzjtunsygqcvmplk/RxmmxzjtunsygqcvmplkApplication.java
@@ -0,0 +1,13 @@
+package kz.example.rxmmxzjtunsygqcvmplk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxmmxzjtunsygqcvmplkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxmmxzjtunsygqcvmplkApplication.class, args);
+	}
+
+}
